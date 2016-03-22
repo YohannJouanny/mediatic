5e8cd6a778ca1617ac22efa83abc382164996b25
rebsau <rebsau@example.com>
2016-03-22 12:13:50 +0100

    Emprunt et Emprunt DAO

--- src/main/java/Model/Media.java
+++ src/main/java/Model/Media.java
@@ -6,10 +6,11 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Media {
-	
+
 	@Id
 	@GeneratedValue
 	private long id;
@@ -19,42 +20,55 @@ public class Media {
 	private String auteur;
 	@Enumerated(EnumType.STRING)
 	private Type type;
-	
-	public enum Type{Livre, CD, DVD;}
-	
-	public Media(){}
-	
-	public Media(String titre,String auteur,Type type){
+	@OneToOne(mappedBy="media")
+	private Emprunt emprunt;
+
+	public enum Type {
+		Livre, CD, DVD;
+	}
+
+	public Media() {
+	}
+
+	public Media(String titre, String auteur, Type type) {
 		this.titre = titre;
 		this.auteur = auteur;
 		this.type = type;
 	}
-	
-	public long getId(){
+
+	public long getId() {
 		return this.id;
 	}
-	
-	//SETTER et GETTER du titre
-	public void setTitre(String t){
+
+	// SETTER et GETTER du titre
+	public void setTitre(String t) {
 		this.titre = t;
 	}
-	public String getTitre(){
+	public String getTitre() {
 		return this.titre;
 	}
-	
-	//SETTER et GETTER de l'auteur
-	public void setAuteur(String a){
+
+	// SETTER et GETTER de l'auteur
+	public void setAuteur(String a) {
 		this.auteur = a;
 	}
-	public String getAuteur(){
+	public String getAuteur() {
 		return this.auteur;
 	}
-	
-	//SETTER et GETTER du type
-	public void setType(Type ty){
+
+	// SETTER et GETTER du type
+	public void setType(Type ty) {
 		this.type = ty;
 	}
-	public Type getType(){
+	public Type getType() {
 		return this.type;
+	}
+
+	// SETTER et GETTER de l'emprunt
+	public void setEmprunt(Emprunt em) {
+		this.emprunt = em;
+	}
+	public Emprunt getEmprunt() {
+		return this.emprunt;
 	}
 }
