125f9b26fc444771800097cab2a6933efb3fad6e
Yohann <yohann.jouanny@gmail.com>
2016-04-12 09:59:14 +0200

    Initialisation de la partie AngularJS

---
+++ app/app.js
@@ -0,0 +1,13 @@
+
+// Création du module ModuleApp avec les dépendances :
+//  - ng-route : pour gérer des URL différentes
+//  - ModuleGlobal : pour avoir les filtres
+angular.module('ModuleApp', ['ngRoute', 'ModuleGlobal']);
+
+// Configuration du module ModuleApp
+// => Injection du Provider du service $route afin de le configurer.
+angular.module('ModuleApp').config(function($routeProvider){
+//		$routeProvider.otherwise({
+//			redirectTo : '/catalogue'
+//		})
+});
