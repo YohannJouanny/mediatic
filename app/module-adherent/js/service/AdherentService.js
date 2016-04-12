89c4dcce434663b504b64dec6d0ab0fc67383f12
Dragoon <kholladi.zino@gmail.com>
2016-04-12 17:07:01 +0200

    Recherche Adherent

---
+++ app/module-adherent/js/service/AdherentService.js
@@ -0,0 +1,86 @@
+
+
+angular.module('ModuleAdherent').service('AdherentService', ['$http',function($http){
+	var self = this;
+	
+	var url = "http://10.34.10.140:8080/resource/adherent.recherche";
+	var promise = undefined;
+	
+	var initPromise = function(){
+		if(promise==undefined){
+			promise = $http.get(url).then(function(response) {
+				var adherents = [];
+				for(var index in response.data){
+					var itemFromServeur = response.data[index];
+					var itemForIHM = {
+						id : itemFromServeur.id,
+						nom : itemFromServeur.nom,
+						prenom : itemFromServeur.prenom,
+						date_naissance : itemFromServeur.date_naissance,
+						cotisation_correcte : itemFromServeur.cotisation_correcte,
+						email : itemFromServeur.email,
+						adresse : {},
+						cotisation : {},
+						age : itemFromServeur.age,
+						emprunt : [],
+						nombre_media : itemFromServeur.nombre_media	
+					};
+					
+				 	for (index in itemFromServeur.emprunt){
+						itemForIHM.emprunt.push(
+								{media:
+									{
+										id: itemFromServeur.emprunt[index].media.id,
+										titre: itemFromServeur.emprunt[index].media.titre
+									},
+										depart: itemFromServeur.emprunt[index].depart,
+										retour: itemFromServeur.emprunt[index].retour
+								}
+						)
+					} 
+					
+					if(itemFromServeur.adresse != undefined){
+						itemForIHM.adresse = {
+							ligne1: itemFromServeur.adresse.ligne1,
+							ligne2: itemFromServeur.adresse.ligne2,
+							codepostal: itemFromServeur.adresse.codepostal,
+							ville: itemFromServeur.adresse.ville	
+						};
+					}
+					
+					if(itemFromServeur.cotisation != undefined){
+						itemForIHM.cotisation = {
+								debut: itemFromServeur.cotisation.debut,
+								fin: itemFromServeur.cotisation.fin,
+								montant: itemFromServeur.cotisation.montant
+						};
+					}
+						
+					adherents.push(itemForIHM);						
+				}	
+				return adherents;
+			});
+		}
+	};
+	
+	self.getList = function(){
+		initPromise();
+		return promise;
+	};
+
+	self.getLivre = function(id){
+		initPromise();
+		return promise.then(function(liste){
+			var index = liste.findIndex(function(item){
+				return item.id === id;
+			});
+			return liste[index];
+		});
+	};
+}]);
+
+
+cotisation_correcte
+:
+true
+
