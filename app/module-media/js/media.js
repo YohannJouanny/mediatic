948828949ebf3cf65d816301079c5d1c09f48375
Yohann <yohann.jouanny@gmail.com>
2016-04-13 12:42:21 +0200

    Correction du merge

--- app/module-media/js/media.js
+++ app/module-media/js/media.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> 57190df... Ajout de la page création média + correction de la page connexion
 // Création du module ModuleMedia
 angular.module('ModuleMedia', ['ngRoute']);
 
@@ -13,23 +10,17 @@ angular.module('ModuleMedia').config(function($routeProvider){
 		controller : 'MediaController',
 		controllerAs : 'medCtrl'
 	});
-<<<<<<< HEAD
 	
 //	$routeProvider.when('/visuMedia', {
 //		templateUrl : './module-media/templates/visualisationMedia.html',
 //		controller : 'VisuMediaController',
 //		controllerAs : 'visuMedCtrl'
 //	});
-	
-=======
->>>>>>> 57190df... Ajout de la page création média + correction de la page connexion
+
 	$routeProvider.when('/media/creation', {
 		templateUrl : './module-media/templates/creationMedia.html',
 		controller : 'CreerMediaController',
 		controllerAs : 'newMediaCtrl'
-<<<<<<< HEAD
-	});
-=======
 	});
 });
->>>>>>> 57190df... Ajout de la page création média + correction de la page connexion
+
