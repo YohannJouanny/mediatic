718cb86f54837156f85d41415bbff9828245c11a
Dragoon <kholladi.zino@gmail.com>
2016-04-13 14:40:29 +0200

    Recherche Adherent

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@
 //  - ModuleGlobal : pour avoir les filtres
 
 
-angular.module('ModuleApp', ['ngRoute', 'ModuleGlobal', 'ModuleMedia', 'ModuleAdherent']);
+angular.module('ModuleApp', ['ngRoute', 'ModuleGlobal', 'ModuleMedia', 'ModuleAdherent', 'FiltreModule']);
 
 // Configuration du module ModuleApp
 // => Injection du Provider du service $route afin de le configurer.
--- app/module-adherent/js/controller/AdherentController.js
+++ app/module-adherent/js/controller/AdherentController.js
@@ -1,13 +1,14 @@
 // Récupération du module des catalogue pour y ajouter le controller
 angular.module('ModuleAdherent').controller('AdherentController', ['$location', '$rootScope', 'AdherentService', function( $location, $rootScope, AdherentService) {
 	var myCtrl = this;
-	
+
 
 	myCtrl.adherents = undefined;
 	
-	AdherentService.getList().then(function(response) {
+	AdherentService.getList({}).then(function(response) {
 		// En cas de succes
 		myCtrl.adherents = response;
+			console.log('_____',myCtrl.adherents);
 	}, function(){
 		// En cas d'erreur
 		myCtrl.adherents = -1;
@@ -23,5 +24,42 @@ angular.module('ModuleAdherent').controller('AdherentController', ['$location', 
 	
 	
 	
+	myCtrl.recherche = function(){
+		var recherche = {
+			id : myCtrl.id,
+			texte : myCtrl.NomEtPrenom
+		}
+		
+
+	AdherentService.getList(recherche).then(function(response){
+			// En cas de succes
+			myCtrl.adherents = response;
+			console.log('_____',myCtrl.adherents);
+
+		}, function(){
+			// En cas d'erreur
+			myCtrl.adherents = -1;
+		})
+	}
+	
+	
+	
+	myCtrl.totalItems = 75;
+	myCtrl.currentPage = 0;
+	myCtrl.maxSize = 5;
+	
+
+	
+	
+	
+	
+	
+
+	
 	
 }]);
+
+
+
+
+	
